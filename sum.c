fcba78d11be8e7d2deb066c3ae14dbbb5d6d0192
student-akriti123 <akritisinha1602@gmail.com>
2023-12-10 04:42:40 +0530

    first commit

---
+++ sum.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int add(int a,int b)
+{
+   return (a+b) ;
+}
+
+int main()
+
+{
+    int a;
+    printf("Enter the value of:");
+    scanf("%d",&a);
+
+    int b;
+    printf("Enter the value of:");
+    scanf("%d",&b);
+
+
+ int sum =add(a,b);
+ printf("%d ",sum);
+
+return 0;
+}
