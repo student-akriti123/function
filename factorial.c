e428deaada1f03b1fba6784f5f8fde065fc6159c
student-akriti123 <akritisinha1602@gmail.com>
2023-12-10 05:25:30 +0530

    first commit

---
+++ factorial.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int factorial(int x)
+{
+     int fact=1;
+    for(int i=2;i<=x;i++)
+    {
+        fact = fact*i;
+    }
+return fact;
+}
+
+
+
+int main()
+{
+
+ int n;
+ printf("Enter the value of:");
+ scanf("%d",&n);
+
+
+ int r;
+ printf("Enter the value of:");
+ scanf("%d",&r);
+
+ int ncr= factorial(n)/(factorial(r)*factorial(n-r));
+
+  printf("%d",ncr) ;
+    return 0;
+}
